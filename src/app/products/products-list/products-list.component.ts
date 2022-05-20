879cd1d0fb1f0c1b316df248a26c251a003732d8
menna-a-a-youssef <menna.a.a.youssef@gmail.com>
2022-05-20 15:09:05 +0200

    added products list

---
+++ src/app/products/products-list/products-list.component.ts
@@ -0,0 +1,40 @@
+import {Component, OnInit} from '@angular/core';
+import { map } from 'rxjs/operators';
+import { Breakpoints, BreakpointObserver } from '@angular/cdk/layout';
+
+@Component({
+  selector: 'app-products-list',
+  templateUrl: './products-list.component.html',
+  styleUrls: ['./products-list.component.css']
+})
+export class ProductsListComponent implements OnInit{
+  products: any[]  = [];
+  constructor() { }
+
+  ngOnInit():void {
+    fetch('https://fakestoreapi.com/products?limit=10')
+      .then(res=>res.json())
+      .then(json=> {this.products = json;  console.log(this.products)} )
+  }
+  // cards = this.breakpointObserver.observe(Breakpoints.Handset).pipe(
+  //   map(({ matches }) => {
+  //     if (matches) {
+  //       return [
+  //         { title: 'Card 1', cols: 1, rows: 1 },
+  //         { title: 'Card 2', cols: 1, rows: 1 },
+  //         { title: 'Card 3', cols: 1, rows: 1 },
+  //         { title: 'Card 4', cols: 1, rows: 1 }
+  //       ];
+  //     }
+  //
+  //     return [
+  //       { title: 'Card 1', cols: 2, rows: 1 },
+  //       { title: 'Card 2', cols: 1, rows: 1 },
+  //       { title: 'Card 3', cols: 1, rows: 2 },
+  //       { title: 'Card 4', cols: 1, rows: 1 }
+  //     ];
+  //   })
+  // );
+
+  //constructor(private breakpointObserver: BreakpointObserver) {}
+}
